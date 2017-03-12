34fa99c7c9eeded5d1118d88c69c1c7d79ca2561
Mohammad Jakir Hossain <jakir_comilla1@yahoo.com>
2017-03-12 12:09:42 +0600

    PHP File

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$word_limit = str_word_count("Hello world!");
+
+if ($word_limit == "9") {
+    echo "Word Limit is = 2";
+} else {
+    echo "Word Limit is not 2";
+}
+echo "<br />";
+
+
+?>
+
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+	
+		
+	</body>
+</html>
